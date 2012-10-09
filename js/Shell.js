26f2bda72273337d0f9f28b17c146215d9d5e8fa
Ben Francis <ben@tola.me.uk>
2012-10-09 23:58:58 +0100

    Load web page, closes #126

--- js/Shell.js
+++ js/Shell.js
@@ -19,15 +19,113 @@
  * <http://www.gnu.org/licenses/>.
  */
 
+/**
+ * Main Shell Object
+ *
+ * Initialises all the other components of Shell.
+ */
 var Shell = {
 
   /**
    * Initialise system toolbar
    */
   init: function shell_init() {
+    WindowToolbar.init();
+    WindowFrame.init();
     SystemToolbar.init();
   }
-}
+};
+
+/**
+ * Window Toolbar
+ *
+ * Responsible for window navigation.
+ */
+var WindowToolbar = {
+
+  /**
+   * Initialises system toolbar.
+   */
+  init: function windowToolbar_init() {
+    this.urlButton = document.getElementById('url_button');
+    this.urlInput = document.getElementById('url_input');
+    this.urlButton.addEventListener('click', WindowFrame.go.bind(WindowFrame));
+    this.urlInput.addEventListener('submit', WindowFrame.go.bind(WindowFrame));
+  }
+
+};
+
+/**
+ * Window Frame
+ *
+ * Manages the window iframe.
+ */
+var WindowFrame = {
+
+  /**
+   * Initialises window.
+   */
+  init: function window_init() {
+    this.frame = document.getElementById('window_frame');
+  },
+
+  /**
+   * Navigates to a web page.
+   */
+  go: function window_go(event) {
+
+    // Stop form submitting & refreshing the page
+    event.preventDefault();
+    var url = WindowToolbar.urlInput.value;
+
+    // Trim whitespace
+    url = url.trim();
+
+    // Prepend http:// if no protocol specified
+    var protocolRegex = /^([a-z]+:)(\/\/)?/i;
+    if (!protocolRegex.exec(url))
+      url = 'http://' + url;
+
+    // Navigate the iframe
+    this.frame.src = url;
+  }
+
+};
+
+/**
+ * System Toolbar
+ *
+ * Contains clock.
+ */
+var SystemToolbar = {
+
+  /**
+   * Initialises system toolbar.
+   */
+  init: function systemToolbar_init() {
+    this.clock = document.getElementById('clock');
+    this.updateClock();
+    window.setInterval(this.updateClock.bind(this), 1000);
+  },
+
+  /**
+   * Updates Clock.
+   */
+  updateClock: function systemToolbar_updateClock() {
+    var date = new Date(),
+    hours = date.getHours() + '', // get hours as string
+    minutes = date.getMinutes() + ''; // get minutes as string
+
+    // pad with zero if needed
+    if (hours.length < 2)
+      hours = '0' + hours;
+    if (minutes.length < 2)
+      minutes = '0' + minutes;
+
+    this.clock.innerHTML = hours + ':' + minutes;
+  }
+
+};
 
 window.addEventListener('load', function shell_onLoad() {
   window.removeEventListener('load', shell_onLoad);
