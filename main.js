10c5d412de0ecde66c5a463f6b0c1f7f0ff0e6d0
Ben Francis <ben@tola.me.uk>
2019-12-06 19:26:42 +0000

    Upgrade Electron and work around fullscreen regression

--- main.js
+++ main.js
@@ -7,37 +7,42 @@
 const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
+const Menu = electron.Menu;
 const path = require('path');
 const url = require('url');
 
 let mainWindow;
 
 function startShell () {
- // Create the main window
- mainWindow = new BrowserWindow({
-   fullscreen: true,
-   webPreferences: {
-     nodeIntegration: true,
-     webviewTag: true
-   }
- });
- // Load shell.html as chrome
- mainWindow.loadURL(url.format({
-   pathname: path.join(__dirname, 'chrome/shell.html'),
-   protocol: 'file:',
-   slashes: true
- }));
+  // Create the main window
+  mainWindow = new BrowserWindow({
+    fullscreen: true,
+    webPreferences: {
+      nodeIntegration: true,
+      webviewTag: true
+    }
+  });
+  
+  // Workaround for https://github.com/electron/electron/issues/21259
+  Menu.setApplicationMenu(null);
 
- // Emitted when the window is closed.
- mainWindow.on('closed', function () {
-   // Dereference the window object
-   mainWindow = null;
-   // Quit Electron
-   app.quit();
- });
+  // Load shell.html as chrome
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'chrome/shell.html'),
+    protocol: 'file:',
+    slashes: true
+  }));
 
- // Uncomment the following line to open developer tools for the main window
- //mainWindow.webContents.openDevTools();
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function () {
+    // Dereference the window object
+    mainWindow = null;
+    // Quit Electron
+    app.quit();
+  });
+
+  // Uncomment the following line to open developer tools for the main window
+  //mainWindow.webContents.openDevTools();
 };
 
 // Start Shell when Electron is ready
